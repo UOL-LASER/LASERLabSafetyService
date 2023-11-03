5322ec3d8be12dcb084255503df5bbdec7b9cc9f
Peter Hamilton <peterhamilton522@gmail.com>
2023-11-03 15:32:04 +0000

    Update

--- src/commands/help.py
+++
@@ -1,7 +0,0 @@
-async def command(message):
-    await message.channel.send('''
-**!labentry** *(Full name)* *(Student ID)*: 
-Upon entry to the lab, please execute this command with the required arguments.
-**!labexit**: 
-Upon exit from the lab, please execute this command to stop the checkup process.
-''')
--- src/commands/labentry.py
+++
@@ -1,5 +0,0 @@
-from modules.userhandler import UserHandling
-
-async def command(message, args):
-    args = args.split(" ")
-    await UserHandling().start(args[0], args[1], message.author.id)
