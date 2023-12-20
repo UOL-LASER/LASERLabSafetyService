9c61ef3552e774bbfb32f87a719510137294d3cd
Peter Hamilton <peterhamilton522@gmail.com>
2023-12-20 20:11:08 +0000

    Initial commit for rewrite

--- src/data/labuser.py
+++
@@ -1,5 +0,0 @@
-class LabUser:
-    def __init__(self, name, studentid, labentrydatetime):
-        self.name = name
-        self.studentid = studentid
-        self.labentrydatetime = labentrydatetime
